b2abec15a794be9f0a134f7e40f2bf7a9e2f65d5
Brock B <wispysparks@gmail.com>
2023-01-09 15:50:11 -0600

    initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,44 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
+import frc.robot.Lib.config.FeedbackConfig;
+import frc.robot.Lib.util.Util;
+
+public class Constants {
+    
+    public static final class Drive {
+        public static final int LFID = 0;
+        public static final int LRID = 1;
+        public static final int RFID = 2;
+        public static final int RRID = 3;
+
+        public static final double diameter = 6;
+        public static final int encoderUnits = 2048;
+        public static final double gearboxRatio = 12.75;
+        public static final double kTrackWidth = Util.inchesToMeters(23.5);
+
+        public static final FeedbackConfig kFeedbackConfig = new FeedbackConfig(FeedbackDevice.IntegratedSensor, encoderUnits, gearboxRatio);
+    }
+
+    // ========================================
+    //    Global Motor Controller Constants
+    // ========================================
+    public static final class MotorController {
+
+        // Status frames are sent over CAN that contain data about the Talon.
+        // They are broken up into different pieces of data and the frequency
+        // at which they are sent can be changed according to your needs.
+        // The period at which their are sent is measured in ms
+
+        public static final int kTalonFrame1Period = 20;  // How often the Talon reports basic info(Limits, limit overrides, faults, control mode, invert)
+        public static final int kTalonFrame2Period = 20;  // How often the Talon reports sensor info(Sensor position/velocity, current, sticky faults, profile)
+        public static final int kTalonFrame3Period = 160;  // How often the Talon reports non selected quad info(Position/velocity, edges, quad a and b pin, index pin)
+        public static final int kTalonFrame4Period = 160;  // How often the Talon reports additional info(Analog position/velocity, temperature, battery voltage, selected feedback sensor)
+        public static final int kTalonFrame8Period = 160;  // How often the Talon reports more encoder info(Talon Idx pin, PulseWidthEncoded sensor velocity/position)
+        public static final int kTalonFrame10Period = 160;  // How often the Talon reports info on motion magic(Target position, velocity, active trajectory point)
+        public static final int kTalonFrame13Period = 160; // How often the Talon reports info on PID(Error, Integral, Derivative)
+
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,132 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.PigeonIMU;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Drive;
+import frc.robot.Lib.Encoder;
+import frc.robot.Lib.util.Util;
+
+
+public class DriveSubsystem extends SubsystemBase{
+
+    private MotorControllerGroup right;
+    private MotorControllerGroup left;
+    private WPI_TalonFX TalonFXL; 
+    private WPI_TalonFX TalonFXLfollow; 
+    private WPI_TalonFX TalonFXR; 
+    private WPI_TalonFX TalonFXRfollow;
+    private final DifferentialDriveKinematics mKinematics = new DifferentialDriveKinematics(Drive.kTrackWidth); 
+    private final DifferentialDriveOdometry odometry = null; // fix
+    private final PigeonIMU mImu = new PigeonIMU(0);
+
+    public DriveSubsystem() {
+        TalonFXL = new WPI_TalonFX(Drive.LFID); 
+        TalonFXLfollow = new WPI_TalonFX(Drive.LRID); 
+        TalonFXR = new WPI_TalonFX(Drive.RFID); 
+        TalonFXRfollow = new WPI_TalonFX(Drive.RRID);
+        TalonFXL.setInverted(true);
+        TalonFXLfollow.setInverted(true);
+        TalonFXLfollow.follow(TalonFXL);
+        TalonFXRfollow.follow(TalonFXR);
+        left =  new MotorControllerGroup(TalonFXL, TalonFXLfollow);
+        right = new MotorControllerGroup(TalonFXR, TalonFXRfollow);
+    }
+
+    @Override
+    public void periodic() {}
+
+    public void setPower(double leftPower, double rightPower){
+        left.set(leftPower);
+        right.set(rightPower);
+    }
+    
+    public double getRightPostition() {
+        return Encoder.toDistance(TalonFXR.getSelectedSensorPosition(), Drive.encoderUnits, Drive.gearboxRatio, Drive.diameter); 
+        
+    }
+    
+    public double getleftPostition() {
+        return Encoder.toDistance(TalonFXL.getSelectedSensorPosition(), Drive.encoderUnits, Drive.gearboxRatio, Drive.diameter); 
+    }
+    
+    public void callibrate() {
+        TalonFXL.setSelectedSensorPosition(0);
+        TalonFXLfollow.setSelectedSensorPosition(0);
+        TalonFXR.setSelectedSensorPosition(0);
+        TalonFXRfollow.setSelectedSensorPosition(0);
+    }
+
+    public void brake() {
+        TalonFXL.setNeutralMode(NeutralMode.Brake);
+        TalonFXLfollow.setNeutralMode(NeutralMode.Brake);
+        TalonFXR.setNeutralMode(NeutralMode.Brake);
+        TalonFXRfollow.setNeutralMode(NeutralMode.Brake);
+    }
+
+    public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+        return new DifferentialDriveWheelSpeeds(getVelocityLeft(), getVelocityRight());
+    }
+
+    public void setVelocity(DifferentialDriveWheelSpeeds speeds) {
+        TalonFXR.set(ControlMode.Velocity, fromVelocity(speeds.rightMetersPerSecond));
+        TalonFXL.set(ControlMode.Velocity, fromVelocity(speeds.leftMetersPerSecond));
+    }
+
+    public void setVelocity(DifferentialDriveWheelSpeeds speeds, double deadband) {
+        if(Util.inRange(speeds.rightMetersPerSecond, deadband)) {
+            TalonFXR.set(ControlMode.PercentOutput, 0);
+        } else {
+            TalonFXR.set(ControlMode.Velocity, fromVelocity(speeds.rightMetersPerSecond));
+        }
+        if(Util.inRange(speeds.leftMetersPerSecond, deadband)) {
+            TalonFXL.set(ControlMode.PercentOutput, 0);
+        } else {
+            TalonFXL.set(ControlMode.Velocity, fromVelocity(speeds.rightMetersPerSecond));
+        }
+    }
+
+    public void setVoltage(double voltsL, double voltsR) {
+        TalonFXL.setVoltage(voltsL);
+        TalonFXR.setVoltage(voltsR);
+    }
+
+    //Maybe change bc it's only checking one motor on the right
+    public double getVelocityRight() {
+        return toVelocity((int)TalonFXR.getSelectedSensorVelocity());
+    }
+
+    //Maybe change bc it's only checking one motor on the left
+    public double getVelocityLeft() {
+        return toVelocity((int)TalonFXL.getSelectedSensorVelocity());
+    }
+
+    public double getVelocity() {
+        return mKinematics.toChassisSpeeds(getWheelSpeeds()).vxMetersPerSecond;
+    }
+
+    public double toVelocity(int velocity) {
+        return Encoder.toVelocity(velocity, Drive.kFeedbackConfig.getEpr(), Drive.kFeedbackConfig.getGearRatio(), Drive.diameter);
+    }
+
+    public double fromVelocity(double velocity) {
+        return Encoder.fromVelocity(velocity, Drive.kFeedbackConfig.getEpr(), Drive.kFeedbackConfig.getGearRatio(), Drive.diameter);
+    }
+
+    public Pose2d getPose() {
+        return odometry.getPoseMeters();
+    }
+
+    public double getHeading() {
+        return Util.boundedAngleDegrees(mImu.getFusedHeading());
+    }
+
+}
