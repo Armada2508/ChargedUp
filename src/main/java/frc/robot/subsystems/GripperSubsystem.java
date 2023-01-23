204c13842d86f253775824e3333be3a7c1765354
Chris <christopp1228@gmail.com>
2023-01-23 16:24:29 -0600

    worked on gripper and wrist subsystems

--- src/main/java/frc/robot/subsystems/GripperSubsystem.java
+++ src/main/java/frc/robot/subsystems/GripperSubsystem.java
@@ -1,7 +1,18 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.fasterxml.jackson.core.io.NumberInput;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Gripper;
 
 public class GripperSubsystem extends SubsystemBase {
     
+    private WPI_TalonFX talonFX = new WPI_TalonFX(Gripper.motorID);
+
+    public void setPower(double powerInput) {
+        talonFX.set(powerInput);
+    }
+
+    //public void setGripperPosition(double )
 }
--- src/main/java/frc/robot/subsystems/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/WristSubsystem.java
@@ -1,7 +1,18 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Wrist;
 
 public class WristSubsystem extends SubsystemBase {
-    
+
+    private WPI_TalonFX talonFX = new WPI_TalonFX(Wrist.motorID);
+
+    public double setWristPosition() {
+
+        
+    }
+
 }
+//add method to set position of the wrist to x ammount of degrees similar to the arm, min degrees = -30, max degrees = 30
