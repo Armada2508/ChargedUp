a072b1133a82f20ab4f46baa926b425532982aef
Wispy Sparks <wispysparks@gmail.com>
2023-02-02 18:27:21 -0600

    nothing

--- src/main/java/frc/robot/commands/Auto/GetConeCommand.java
+++ src/main/java/frc/robot/commands/Auto/GetConeCommand.java
@@ -1,9 +1,15 @@
 package frc.robot.commands.Auto;
 
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
 public class GetConeCommand {
     
+    private SequentialCommandGroup group;
+
     public GetConeCommand() {
-        
+        group = new SequentialCommandGroup(
+
+        );
     }
 
 }
