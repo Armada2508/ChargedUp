256fd2f2f9c6535d035314ad3947a16104168fa4
Chris <christopp1228@gmail.com>
2023-02-02 18:43:08 -0600

    Started Get cube command

---
+++ src/main/java/frc/robot/commands/Auto/GetCubeCommand.java
@@ -0,0 +1,14 @@
+package frc.robot.commands.Auto;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class GetCubeCommand {
+    
+    private SequentialCommandGroup group;
+
+    public GetCubeCommand() {
+        group = new SequentialCommandGroup(
+
+        );
+    }
+}
