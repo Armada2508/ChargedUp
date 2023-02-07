5906456335541ea728e31cafa6f2bd34f15dd7a9
Wispy Sparks <wispysparks@gmail.com>
2023-02-07 15:59:44 -0600

    fix cone pipeline garbage

--- src/main/java/frc/robot/commands/Driving/SeekCommand.java
+++ src/main/java/frc/robot/commands/Driving/SeekCommand.java
@@ -2,24 +2,17 @@ package frc.robot.commands.Driving;
 
 import com.ctre.phoenix.sensors.PigeonIMU;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.PhotonSubsystem;
-import frc.robot.subsystems.VisionSubsystem.Target;
+import frc.robot.subsystems.PhotonSubsystem.Target;
 
 public class SeekCommand extends SequentialCommandGroup {
 
     public SeekCommand(DriveSubsystem driveSubsystem, PhotonSubsystem photonSubsystem, PigeonIMU pigeon, Target target, double distanceFromTargetInches) {
-        Command command = new InstantCommand();
-        if (target == Target.CONE) command = new InstantCommand(photonSubsystem::correctConePipeline);
         addCommands(
             new AutoTurnCommand(photonSubsystem::getTargetYaw, driveSubsystem, pigeon),
-            command,
-            new WaitCommand(0.1),
             new DriveUntilCommand(driveSubsystem, photonSubsystem, target),
             new AutoDriveCommand(() -> photonSubsystem.getDistanceToTargetInches(target) - distanceFromTargetInches, driveSubsystem)
         );
