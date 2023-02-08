1384e8ce2fb0b8e924585f6c94a7fa94d6cc7669
Chris <christopp1228@gmail.com>
2023-02-07 18:56:18 -0600

    about 50% completed with autopickup

--- src/main/java/frc/robot/commands/Auto/AutoPickupCommand.java
+++ src/main/java/frc/robot/commands/Auto/AutoPickupCommand.java
@@ -3,32 +3,30 @@ package frc.robot.commands.Auto;
 import com.ctre.phoenix.sensors.PigeonIMU;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Constants.Arm;
-import frc.robot.commands.Arm.ArmCommand;
-import frc.robot.commands.Arm.GripperCommand;
-import frc.robot.commands.Arm.WristCommand;
-import frc.robot.commands.Driving.SeekCommand;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.GripperSubsystem;
 import frc.robot.subsystems.PhotonSubsystem;
-import frc.robot.subsystems.VisionSubsystem.Target;
 import frc.robot.subsystems.WristSubsystem;
 
 public class AutoPickupCommand extends SequentialCommandGroup {
-    
+
     public AutoPickupCommand(PhotonSubsystem photonSubsystem, DriveSubsystem driveSubsystem, PigeonIMU pigeon, ArmSubsystem ArmSubsystem, WristSubsystem WristSubsystem, GripperSubsystem GripperSubsystem) {
         addCommands(
             new GripperCommand(0, GripperSubsystem),
             new ArmCommand(Arm.minDegrees, ArmSubsystem), 
-            //!
+            new ConditionalCommand(
+                
+            new InstantCommand((() -> photonSubsystem.getDistanceToTargetInches(), photonSubsystem(setPipeline(photonSubsystem.getPipeline())), /* On True */ 
+            null, /* On False */ 
+            photonSubsystem::hasTargets /* Boolean Supplier */)),
+            
             new SeekCommand(driveSubsystem, photonSubsystem, pigeon, Target.CONE, 12),
             new ArmCommand(20, ArmSubsystem), // theta value is a guess, change as needed
             new WristCommand(10, WristSubsystem), // theta value is a guess, change as needed
             new GripperCommand(1, GripperSubsystem),
             //!
-            new ArmCommand(Arm.minDegrees, ArmSubsystem)
+            new ArmCommand(Arm.minDegrees, ArmSubsystem) 
         );
     }
-
 }
