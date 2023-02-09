cbdb8b961de495c646294e69f489a1d00c16c337
Chris <christopp1228@gmail.com>
2023-02-09 16:05:33 -0600

    pickup

--- src/main/java/frc/robot/commands/Auto/AutoPickupCommand.java
+++ src/main/java/frc/robot/commands/Auto/AutoPickupCommand.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix.sensors.PigeonIMU;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.Arm;
 import frc.robot.Constants.Vision;
 import frc.robot.commands.Arm.ArmCommand;
@@ -22,10 +23,21 @@ public class AutoPickupCommand extends SequentialCommandGroup {
 
     private Target lastTarget = Target.CONE;
 
+    private double cubeDistance;
+    private double coneDistance;
+
     public AutoPickupCommand(PhotonSubsystem photonSubsystem, DriveSubsystem driveSubsystem, PigeonIMU pigeon, ArmSubsystem ArmSubsystem, WristSubsystem WristSubsystem, GripperSubsystem GripperSubsystem) {
         addCommands(
             new GripperCommand(0, GripperSubsystem),
-            new ArmCommand(Arm.minDegrees, ArmSubsystem), 
+            new ArmCommand(Arm.minDegrees, ArmSubsystem),
+            new SequentialCommandGroup(
+                new InstantCommand(() -> photonSubsystem.setPipeline(Vision.cubePipeline), photonSubsystem),
+                new WaitCommand(0.1),
+                new InstantCommand(() -> coneDistance = photonSubsystem.getDistanceToTargetInches(Target.CONE)),
+                new InstantCommand(() -> photonSubsystem.setPipeline(Vision.coneLandscapePipeline), photonSubsystem),
+                new WaitCommand(0.1),
+                new InstantCommand(() -> cubeDistance = photonSubsystem.getDistanceToTargetInches(Target.CONE))
+            ),
             new ConditionalCommand(
                 new SequentialCommandGroup(
                     new SeekCommand(driveSubsystem, photonSubsystem, pigeon, () -> getTarget(photonSubsystem), 12),
@@ -40,15 +52,27 @@ public class AutoPickupCommand extends SequentialCommandGroup {
         );
     }
 
-    private Target getTarget(PhotonSubsystem photonSubsystem) {
-        Target target = Target.CONE;
-        if (photonSubsystem.getPipeline() == Vision.cubePipeline) {
-            photonSubsystem.getDistanceToTargetInches(Target.CUBE);
-            target = Target.CUBE;
+    //~ new WaitCommand(seconds),
+    //// switch to cube pipeline, wait 0.1 seconds, grab distance and store in a var
+    //// switch to cone pipeline, wait 0.1 seconds, grab distance and store in a var
+    // getTarget(coneDistance, cubeDistance)
+    // check if the distances are NaN from Double.NaN. 
+    // if one is NaN and one isn't, then seek to that one
+    // if both are NaN do nothing,
+    // if both have distances go to the one closer
+
+    private Target getTarget(double coneDistance, double cubeDistance, PhotonSubsystem photonSubsystem) {
+        if ((cubeDistance == Double.NaN) && (coneDistance != Double.NaN)) {
+            return Target.CUBE;
         }
-        else {
-            photonSubsystem.getDistanceToTargetInches(Target.CONE);
-            target = Target.CONE;
+        else if ((cubeDistance != Double.NaN) && (coneDistance == Double.NaN)) {
+            return Target.CONE;
+        }
+        else if ((cubeDistance != Double.NaN) && (coneDistance != Double.NaN)) {
+            
+        }
+        else { /*both cubeDistance and coneDistance are NaN */
+
         }
         lastTarget = target;
         return target;
