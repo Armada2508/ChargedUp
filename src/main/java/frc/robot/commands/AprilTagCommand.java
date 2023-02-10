760cf0414c396ae375306d0ba958115faf7a3109
Chris <christopp1228@gmail.com>
2023-02-09 18:41:49 -0600

    began AprilTagCommand

---
+++ src/main/java/frc/robot/commands/AprilTagCommand.java
@@ -0,0 +1,11 @@
+package frc.robot.commands;
+
+public class AprilTagCommand {
+    
+    
+}
+
+
+// grab data from photon vision on where we are relative to april tag - methods are maybe already in the subsystem might have to make them
+// using trajectories go to somewhere in front of the april tag use methods from FollowTrajectory.java to follow it
+// can research trajectories and poses from wpilib docs, can research april tag stuff on photonvision docs
