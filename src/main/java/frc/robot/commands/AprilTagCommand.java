73f5d9f49cf52e859c629fea0aa92bc2326e4d5a
Chris <christopp1228@gmail.com>
2023-02-11 13:33:29 -0600

    added constants for april tags

--- src/main/java/frc/robot/commands/AprilTagCommand.java
+++ src/main/java/frc/robot/commands/AprilTagCommand.java
@@ -1,11 +1,17 @@
 package frc.robot.commands;
 
+
 public class AprilTagCommand {
     
     
 }
 
 
-// grab data from photon vision on where we are relative to april tag - methods are maybe already in the subsystem might have to make them
-// using trajectories go to somewhere in front of the april tag use methods from FollowTrajectory.java to follow it
-// can research trajectories and poses from wpilib docs, can research april tag stuff on photonvision docs
+/*  grab data from photon vision on where we are relative to april tag - methods are maybe already in the subsystem might have to make them
+    using trajectories go to somewhere in front of the april tag use methods from FollowTrajectory.java to follow it
+    can research trajectories and poses from wpilib docs, can research april tag stuff on photonvision docs
+
+    
+
+    & assume we have targeted onto the april tag
+*/
